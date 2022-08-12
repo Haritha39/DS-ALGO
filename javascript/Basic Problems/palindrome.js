afc2be82fc8d1d13ab08b8dd258f5b209ad9d96d
Haritha <haritha.kommireddy@xcubelabs.com>
2022-08-12 14:09:49 +0530

    palindrom and output with ctrl D

--- javascript/Basic Problems/palindrome.js
+++ javascript/Basic Problems/palindrome.js
@@ -9,20 +9,27 @@ let currentLine = 0;
 
 process.stdin.on('data', inputStdin => {
     inputString += inputStdin;
-    console.log(inputString , typeof inputStdin);
 });
 
 process.stdin.on('end', _ => {
     inputString = inputString.replace(/\s*$/, '')
         .split('\n')
         .map(str => str.replace(/\s*$/, ''));
-
     main();
 });
+
 function readLine() {
     return inputString[currentLine++];
 }
 function main() {
-    const n = parseInt(readLine(),10);
-    console.log(n);
+    // const n = parseInt(readLine(),10);
+
+    const n = readLine();
+    let reversed = n.split('').reverse().join('');
+    console.log('reverse ', reversed);
+    if(n === reversed) {
+        console.log('palindrome');
+    } else {
+        console.log('not a palindrome');
+    }
 }
